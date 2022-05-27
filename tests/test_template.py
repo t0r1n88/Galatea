85d6a6a80531e44a4fa3959935bcdb6f1d7b316b
Oleg Budaev <t0r1n88@mail.ru>
2022-05-27 15:25:17 +0800

    л

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,10 +1,9 @@
 import pytest
-import pandas
+import pandas as pd
 # для корректной проверки на тип колонок в датафрейме
 from pandas.api.types import is_object_dtype, is_numeric_dtype, is_bool_dtype
 import os
 from Galatea_main import processing_data
-
 
 PATH_FOLDER_DATA = 'c:/Users/1/PycharmProjects/Galatea/output/'
 
@@ -16,24 +15,25 @@ class TestCorrectFIle:
     """
     Класс для проверки корректности итогового файла(правильные колонки, размер и пр.)
     """
-    def test_shape_df(self,test_df):
+
+    def test_shape_df(self, test_df):
         """
         Дано: датафрейм, результат работы функции processing_data
         Когда: датафрейм перед сохранением в файл
         Тогда: количество колонок в файле должно быть 4
         """
-        assert test_df.shape[1] == 4,"Количество колонок в датафрейме должно быть равным 4"
+        assert test_df.shape[1] == 4, "Количество колонок в датафрейме должно быть равным 4"
 
-    def test_name_columns(self,test_df):
+    def test_name_columns(self, test_df):
         """
         Дано: датафрейм, результат работы функции processing_data
         Когда: датафрейм перед сохранением в файл
         Тогда: Названия колонок должны совпадать с образцом
         """
         # не забывай что test_df.columns это индекс и для сравнения со списком его нужно превратить в список
-        assert list(test_df.columns) == ['ФИО','Серия паспорта','Номер паспорта','Код подразделения']
+        assert list(test_df.columns) == ['ФИО', 'Серия паспорта', 'Номер паспорта', 'Код подразделения']
 
-    def test_type_columns(self,test_df):
+    def test_type_columns(self, test_df):
         """
         Дано: датафрейм, результат работы функции processing_data
         Когда: датафрейм перед сохранением в файл
@@ -43,7 +43,6 @@ class TestCorrectFIle:
         assert is_object_dtype(test_df['ФИО'])
         assert is_numeric_dtype(test_df['Серия паспорта'])
         assert not is_bool_dtype(test_df['Номер паспорта'])
-
 
 
 @pytest.mark.skip(reason='Пока не нужно')
@@ -71,11 +70,4 @@ class TestCorrectCreateFile:
         """
         # test_df = pd.read_excel(f'{PATH_FOLDER_DATA}{FILE_DATA_INCORRECT}')
         test_df = pd.read_excel(f'{PATH_FOLDER_DATA}{FILE_DATA_XLSX}')
-        assert  isinstance(test_df,pandas.core.frame.DataFrame),"Проверяем можно ли сгенерированный файл в пандас"
-
-
-
-
-
-
-
+        assert isinstance(test_df, pd.core.frame.DataFrame), "Проверяем можно ли сгенерированный файл в пандас"
